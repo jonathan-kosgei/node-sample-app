7f570153a17fd54a83cb9174fe3e8fe93054ac89
Jonathan <jonathan@saharacluster.com>
2018-01-17 18:42:03 +0300

    Infinite loop

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-new Promise((resolve, reject) => {
+/*new Promise((resolve, reject) => {
     console.log('started')
        // never resolves or rejects
     })
+*/
+function * makeCounter () {
+  let i = 0;
+  while (true) {
+    yield ++i;
+  }
+}
+makeCounter()
